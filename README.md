ec277b747ffc13335327e2c61e354d87e79da92f
project-palleon <112717308+project-palleon@users.noreply.github.com>
2022-09-02 18:25:57 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# PALLEON input-looping
+
+> This is a repository that's part of the Palleon project, which in turn is a part of the SoC 2022.
+>
+> This project is still very much WIP, so everything is subject to radical change as I have to
+> adjust everything to new requirements that come up on the way of developing.
+>
+> So I am sorry for everyone who has to look at this code. It will get better - I hope...
+
+## How it works
+
+1. repetitively loads another frame of a specified movie file
+2. on the core's request it provides the last read frame
+
+## Installation
+
+1. install python 3 (I am using version 3.10, I have not tested prior version)
+2. install requirements from requirements.txt
+3. start core
---
+++ simple_socket.py
@@ -0,0 +1,59 @@
+import socket
+import struct
+
+
+# todo make shared library
+
+class SocketNotConnectedException(Exception):
+    pass
+
+
+class SimpleSocket:
+    # only supports tcp
+
+    def __init__(self, host, port):
+        self._host = host
+        self._port = port
+
+        self._socket = None
+
+    def connect(self):
+        # force reconnect
+        self.close()
+
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._socket.connect((self._host, self._port))
+
+    def __enter__(self):
+        if not self._socket:
+            self.connect()
+        return self
+
+    def close(self):
+        if self._socket:
+            try:
+                self._socket.close()
+            except:
+                pass
+        self._socket = None
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    def recv_exactly(self, size):
+        if not self._socket:
+            raise SocketNotConnectedException("cant receive without connection")
+
+        buffer = b""
+        while size > 0:
+            received = self._socket.recv(min(size, 1024))
+            buffer += received
+            size -= len(received)
+        return buffer
+
+    def sendall(self, data):
+        self._socket.sendall(data)
+
+    def recv_based_on_32bit_integer(self):
+        length = struct.unpack("<i", self.recv_exactly(4))[0]
+        return self.recv_exactly(length)
